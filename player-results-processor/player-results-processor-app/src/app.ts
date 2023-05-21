6c2cea5b3242c4ed4a282c8bbb1229a9d93757f7
Andrew Shek <andrewshek@Andrews-MacBook-Pro-4.local>
2023-05-21 22:28:50 +1000

    feat: wip

--- player-results-processor/player-results-processor-app/src/app.ts
+++ player-results-processor/player-results-processor-app/src/app.ts
@@ -1,17 +1,24 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import DynamoDb from 'aws-sdk/clients/dynamodb';
-import { DataMapper } from '@aws/dynamodb-data-mapper';
-import { getApiResponse } from './common';
+import { DynamoDBStreamEvent } from 'aws-lambda';
+import logger from './logger';
+// import DynamoDb from 'aws-sdk/clients/dynamodb';
+// import { DataMapper } from '@aws/dynamodb-data-mapper';
+// import { getApiResponse } from './common';
 
-export const playerResultsProcessorHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const client = getDynamoDbClient();
-    const mapper = new DataMapper({ client });
-
-    return getApiResponse(400, JSON.stringify({ error: 'invalid request' }));
+export const playerResultsProcessorHandler = async (event: DynamoDBStreamEvent) => {
+    try {
+        event.Records.map((record) => {
+            logger.info('incoming stream');
+            logger.info(record);
+        });
+        return;
+    } catch (error) {
+        console.log('error', error);
+        return;
+    }
 };
 
-const getDynamoDbClient = (): DynamoDb => {
-    return process.env.DYNAMODB_ENDPOINT
-        ? new DynamoDb({ endpoint: process.env.DYNAMODB_ENDPOINT, region: 'ap-southeast-2' })
-        : new DynamoDb({ region: process.env.AWS_REGION });
-};
+// const getDynamoDbClient = (): DynamoDb => {
+//     return process.env.DYNAMODB_ENDPOINT
+//         ? new DynamoDb({ endpoint: process.env.DYNAMODB_ENDPOINT, region: 'ap-southeast-2' })
+//         : new DynamoDb({ region: process.env.AWS_REGION });
+// };
